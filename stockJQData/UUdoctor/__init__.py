a9204becd9ed0f5979ff58505f3707ba680a389e
minp <king101125s@gmail.com>
2019-06-22 17:07:19 +0800

    删除无用的文件

---
+++ stockJQData/UUdoctor/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019-06-21 15:59
+# @Author  : minp
+# @contact : king101125s@gmail.com
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm
