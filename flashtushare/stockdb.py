b33ad5a265855b995824bced7e8461def3775195
minp <king101125s@gmail.com>
2019-07-04 20:46:02 +0800

    增加Dockerfile

--- flashtushare/stockdb.py
+++ flashtushare/stockdb.py
@@ -6,3 +6,16 @@
 # @Site    : 
 # @File    : stockdb.py
 # @Software: PyCharm
+
+
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["runoobdb2"]
+mycol = mydb["sites"]
+
+mydict = {"name": "RUNOOB", "alexa": "10000", "url": "https://www.runoob.com"}
+
+x = mycol.insert_one(mydict)
+print(x)
+print(x)
